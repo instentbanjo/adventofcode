1cf64b4063492db1340fe2aa370ba6929cd50c33
instentbanjo <instentbanjo@DESKTOP-MQ3DSSK.>
2025-03-18 17:48:53 +0100

    typing-error correction

--- 2015/Day5/Day5.cs
+++ 2015/Day5/Day5.cs
@@ -9,6 +9,7 @@ public class Day5
     {
       Console.WriteLine("---5---");
       Part1();
+      Part2();
     }
 
     static void Part1(){
@@ -17,7 +18,7 @@ public class Day5
       {
         if (GetNumberOfVowels(word) >= 3)
         {
-          if (ContainsDublicates(word))
+          if (ContainsDuplicates(word))
           {
             if (!DoesContainCertainPairs(new string[] { "ab", "cd", "pq", "xy"} ,word))
             {
@@ -29,20 +30,37 @@ public class Day5
       Console.WriteLine(naughtylessWords.Count);
     }
 
+    static void Part2(){
+      Console.WriteLine(GetDuplicates("hallo")[0]);
+    }
+
     static int GetNumberOfVowels(string word){
       HashSet<char> vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
       return word.Count(c => vowels.Contains(c));
     }
 
-    static bool ContainsDublicates(string word){
-      bool containsDublicates = false;
+    static bool ContainsDuplicates(string word){
+      bool containsDuplicates = false;
       for (int i = 1; i < word.Length; i++){
         if (word[i]==word[i-1])
         {
-          containsDublicates = true;
+          containsDuplicates = true;
         }
       }
-      return containsDublicates;
+      return containsDuplicates;
+    }
+
+    static List<string> GetDuplicates(string word)
+    {
+        List<string> duplicates = new List<string>();
+        for (int i = 1; i < word.Length; i++)
+        {
+            if (word[i] == word[i - 1])
+            {
+                duplicates.Add($"{word[i]}{word[i - 1]}"); 
+            }
+        }
+        return duplicates;
     }
 
     static bool DoesContainCertainPairs(string[] pairs, string word)
