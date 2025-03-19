12fe3d62bf1a4b1b17b808a9277ffd5e31f5b30f
instentbanjo <instentbanjo@DESKTOP-MQ3DSSK.>
2025-03-19 11:28:39 +0100

    day 5 part 2 done

--- 2015/Day5/Day5.cs
+++ 2015/Day5/Day5.cs
@@ -31,7 +31,17 @@ public class Day5
     }
 
     static void Part2(){
-      Console.WriteLine(GetDuplicates("hallo")[0]);
+      List<string> naughtylessWords = new List<string>();
+      // string word = "qjhvtzxqqjkmpbp";
+      foreach (string word in _input){
+          // Console.WriteLine(HasDuplicatePairsWOOverlap(word));
+          // Console.WriteLine(HasRepetitionEveryOther(word));
+        if (HasDuplicatePairsWOOverlap(word) && HasRepetitionEveryOther(word))
+        {
+          naughtylessWords.Add((string)word.Clone());
+        }
+      }
+      Console.WriteLine(naughtylessWords.Count);
     }
 
     static int GetNumberOfVowels(string word){
@@ -50,18 +60,46 @@ public class Day5
       return containsDuplicates;
     }
 
-    static List<string> GetDuplicates(string word)
+    static List<string> GetPairs(string word)
     {
         List<string> duplicates = new List<string>();
         for (int i = 1; i < word.Length; i++)
         {
-            if (word[i] == word[i - 1])
-            {
-                duplicates.Add($"{word[i]}{word[i - 1]}"); 
-            }
+          duplicates.Add($"{word[i-1]}{word[i]}"); 
         }
         return duplicates;
     }
+
+    static bool HasDuplicatePairsWOOverlap(string word)
+    {
+        List<string> pairs = GetPairs(word); 
+        foreach (string pair in pairs)
+        {
+            string pattern = @"(" + System.Text.RegularExpressions.Regex.Escape(pair) + @").*?\1";
+            System.Text.RegularExpressions.Regex rg = new System.Text.RegularExpressions.Regex(pattern);
+            if (rg.IsMatch(word))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static bool HasRepetitionEveryOther(string word){
+      List<string> pairs = GetPairs(word);
+      for (int i = 0; i < pairs.Count; i++){
+        List<int> indexesOfPair = word.AllIndexesOf(pairs[i]);
+        for (int y = 0; y < indexesOfPair.Count; y++)
+        {
+          if (indexesOfPair[y] + 2 < word.Length && word[indexesOfPair[y]+2] == pairs[i][0])
+          {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
 
     static bool DoesContainCertainPairs(string[] pairs, string word)
     {
@@ -71,5 +109,22 @@ public class Day5
         }
         return false; 
     }
+
+}
+
+public static class IndexOfExtension
+{
+  // stackoverflow.com/questions/2641326/finding-all-positions-of-substring-in-a-larger-string-in-c-sharp
+  public static List<int> AllIndexesOf(this string str, string value) {
+      if (String.IsNullOrEmpty(value))
+          throw new ArgumentException("the string to find may not be empty", "value");
+      List<int> indexes = new List<int>();
+      for (int index = 0;; index += value.Length) {
+          index = str.IndexOf(value, index);
+          if (index == -1)
+              return indexes;
+          indexes.Add(index);
+      }
+  }
 
 }
