b1be17c78f4069f48796e47a9c3534729122f7d0
instentbanjo <instentbanjo@DESKTOP-MQ3DSSK.>
2025-03-19 13:57:39 +0100

    d6p1

---
+++ 2015/Day6/Day6.cs
@@ -0,0 +1,74 @@
+﻿namespace _2015;
+
+public class Day6
+{
+    
+    private static string[] _input =  File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../Day6/input.txt"));
+    private static bool[,] _lightGrid = new bool[1000,1000];
+
+    static Day6()
+    {
+      Console.WriteLine("---6---");
+      Part1();
+    }
+
+    static void Part1(){
+      foreach (string input in _input)
+      {
+        ProcessLine(input);
+      }
+      Console.WriteLine(CTVIM());
+    }
+
+    static void ProcessLine(string line){
+      string[] arr = line.Split(" ");
+      if (arr[0]=="turn")
+      {
+        Turn(arr[1]=="on", CSITI(arr[2]), CSITI(arr[4]));
+      }else
+      {
+        Toggle(CSITI(arr[1]), CSITI(arr[3]));
+      }
+
+    }
+    
+    static void Turn(bool value, int[] rangeStart,int[] rangeEnd){
+      for (int i = rangeStart[0]; i <= rangeEnd[0]; i++)
+      {
+        for (int y = rangeStart[1]; y <= rangeEnd[1]; y++)
+        {
+          _lightGrid[i,y] = value;
+        }
+      }
+    }
+
+    static void Toggle(int[] rangeStart,int[] rangeEnd){
+      for (int i = rangeStart[0]; i <= rangeEnd[0]; i++)
+      {
+        for (int y = rangeStart[1]; y <= rangeEnd[1]; y++)
+        {
+          _lightGrid[i,y] = !_lightGrid[i,y];
+        }
+      }
+
+    }
+
+    //CountTrueValuesInMatrix
+    static int CTVIM(){
+      int count = 0;
+      for (int i = 0; i < _lightGrid.GetLength(0); i++)
+      {
+        for (int y = 0; y < _lightGrid.GetLength(1); y++)
+        {
+          if (_lightGrid[i,y]) count++;
+        }
+      }
+      return count;
+    }
+
+    //CommaSeperatedIntstringToIntarray
+    static int[] CSITI(string intstring){
+      return [int.Parse(intstring.Split(",")[0]),int.Parse(intstring.Split(",")[1])];
+    }
+
+}
--- 2015/Program.cs
+++ 2015/Program.cs
@@ -5,3 +5,4 @@ Day2 day2 = new Day2();
 Day3 day3 = new Day3();
 Day4 day4 = new Day4();
 Day5 day5 = new Day5();
+Day6 day6 = new Day6();
